470f843cfe299c92f0338711f46fb13d7405e05b
Rick <rickatherton316@gmail.com>
2022-05-19 13:40:06 +0000

    Create HTML code for post_detail where the user clicks to see more detail, and fix several pylint warnings

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,8 @@
+"""
+Standard Django for database models
+Standard Django Authentication model
+Cloudinary as cloud based image repository
+"""
 from django.db import models
 from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
@@ -5,22 +10,26 @@ from cloudinary.models import CloudinaryField
 
 # Global Variables
 STATUS = ((0, "Draft"), (1, "Published"))
-PODCAST_CATEGORY = ((0, "Latest"), (1, "News"), (2, "Life"), (3, "Music"), (4, "Sport"), (5, "Technology"))
+PODCAST_CATEGORY = ((0, "Latest"), (1, "News"), (2, "Life"),
+                    (3, "Music"), (4, "Sport"), (5, "Technology"))
 
 
 class Post(models.Model):
     title = models.CharField(max_length=200, unique=True, primary_key=True)
     slug = models.SlugField(max_length=200, unique=True)
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="blog_post")
+    author = models.ForeignKey(User, on_delete=models.CASCADE,
+                               related_name="blog_posts")
     category = models.IntegerField(choices=PODCAST_CATEGORY, default=0)
     created_on = models.DateTimeField(auto_now_add=True)
     updated_on = models.DateTimeField(auto_now=True)
     content = models.TextField()
-    excerpt = models.TextField(max_length=180)
-    podcast_url = models.URLField(max_length=200, help_text="Please add the podcasts web address")
+    excerpt = models.TextField(max_length=180, blank=True)
+    podcast_url = models.URLField(max_length=200, help_text="Please add the \
+                                  podcasts web address")
     featured_image = CloudinaryField('image', default='placeholder')
     status = models.IntegerField(choices=STATUS, default=0)
-    likes = models.ManyToManyField(User, related_name='blog_likes', blank=True)
+    likes = models.ManyToManyField(User, related_name='blogpost_like',
+                                   blank=True)
 
     class Meta:
         ordering = ['-created_on']
@@ -29,16 +38,22 @@ class Post(models.Model):
         return self.title
     
     def number_of_likes(self):
-        return self.likes.count
+        """
+        Counts number of blog post likes
+        Returns number of blog post likes
+        """
+        return self.likes.count()
 
-    
+
 class Comment(models.Model):
-    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='blog_comments')
+    post = models.ForeignKey(Post, on_delete=models.CASCADE,
+                             related_name='comments')
     username = models.ForeignKey(User, on_delete=models.CASCADE)
     message = models.TextField(help_text="Add your comment here")
     created_on = models.DateTimeField(auto_now_add=True)
     approved = models.BooleanField(default=False)
-    likes = models.ManyToManyField(User, related_name='comment_likes', blank=True)
+    likes = models.ManyToManyField(User, related_name='comment_likes', 
+                                   blank=True)
 
     class Meta:
         ordering = ['created_on']
@@ -47,6 +62,8 @@ class Comment(models.Model):
         return f'Comment {self.message} by {self.username}'
     
     def number_of_likes(self):
-        return self.likes.count
-
-
+        """
+        Counts number of comment likes
+        Returns number of comment likes
+        """
+        return self.likes.count()
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,7 @@
+"""
+Standard Django imports
+Import Post model
+"""
 from django.shortcuts import render, get_object_or_404
 from django.views import generic, View
 from .models import Post
@@ -5,17 +9,16 @@ from .models import Post
 
 class PostList(generic.ListView):
     model = Post
-    queryset = Post.objects.filter(status=1).order_by('-created_on')
+    queryset = Post.objects.filter(status=1).order_by("-created_on")
     template_name = "index.html"
     paginate_by = 6
 
 
 class PostDetail(View):
-
     def get(self, request, slug, *args, **kwargs):
         queryset = Post.objects.filter(status=1)
         post = get_object_or_404(queryset, slug=slug)
-        comments = post.comment.filter(approved=True).order_by("-created_on")
+        comments = post.comments.filter(approved=True).order_by("-created_on")
         liked = False
         if post.likes.filter(id=self.request.user.id).exists():
             liked = True
