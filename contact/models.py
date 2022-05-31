9ae5437c25318a7612661cc9732d6283a40feb9d
Rick <rickatherton316@gmail.com>
2022-05-31 14:41:29 +0000

    Finalise styles for Contact page, update button style to be used generically throughout sit

--- contact/models.py
+++ contact/models.py
@@ -6,5 +6,5 @@ class Contact(models.Model):
     name = models.CharField(max_length=50)
     email = models.EmailField(max_length=50)
     subject = models.CharField(max_length=200)
-    message = models.TextField(max_length=2000, help_text="Add your message here")
+    message = models.TextField(max_length=2000)
     date_submitted = models.DateTimeField(default=timezone.now)
