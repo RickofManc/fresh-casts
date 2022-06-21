703c8affb693230de560c4ba9575f4715a759d25
Rick <rickatherton316@gmail.com>
2022-06-21 21:37:42 +0000

    General updates to several files from performing Testing; Code Validation, WAVE tool

--- contact/admin.py
+++ contact/admin.py
@@ -11,6 +11,14 @@ class ContactAdmin(admin.ModelAdmin):
     Displays the fields and values
     for Contact Model in admin panel.
     """
-    list_display = ('name', 'email', 'subject', 'message', 'date_submitted', 'contact_status')
-    search_fields = ['name', 'subject', 'message', 'date_submitted', 'contact_status']
-    list_filter = ('name', 'date_submitted', 'contact_status')
+    list_display = (
+        'name', 'email', 'subject',
+        'message', 'date_submitted', 'contact_status'
+        )
+    search_fields = [
+        'name', 'subject', 'message',
+        'date_submitted', 'contact_status'
+        ]
+    list_filter = (
+        'name', 'date_submitted', 'contact_status'
+        )
