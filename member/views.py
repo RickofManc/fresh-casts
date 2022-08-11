aaced8b7ab82f8c983b68b270560f9c0fa5001ab
Rick <rickatherton316@gmail.com>
2022-08-11 14:29:33 +0000

    Add SuccessMessageMixin to class based views to improve user feedback and experience when updating profile information

--- member/views.py
+++ member/views.py
@@ -5,6 +5,7 @@ from django.views import View
 from django.views.generic import CreateView, UpdateView
 from django.contrib.auth.views import PasswordChangeView
 from django.contrib.auth import get_user_model
+from django.contrib.messages.views import SuccessMessageMixin
 from django.urls import reverse_lazy
 from blog.models import Category
 from .forms import EditProfileForm, PasswordChangingForm
@@ -15,7 +16,7 @@ from .forms import RegisterUserForm, LoginForm
 User = get_user_model()
 
 
-class SignUpView(CreateView):
+class SignUpView(SuccessMessageMixin, CreateView):
     """
     Displays SignUp page.
     gets : requested template by name
@@ -26,6 +27,8 @@ class SignUpView(CreateView):
     form_class = RegisterUserForm
     template_name = "signup.html"
     success_url = reverse_lazy("home")
+    success_message = "Thank you for joining our community! \
+                       You can now like and add blog posts"
 
     def get_object(self, queryset=None):
         """Returns user object"""
@@ -63,7 +66,7 @@ class LoginView(View):
         return context
 
 
-class UserEditView(UpdateView):
+class UserEditView(SuccessMessageMixin, UpdateView):
     """
     Displays Edit Profile page.
     gets : requested template by name
@@ -73,6 +76,7 @@ class UserEditView(UpdateView):
     form_class = EditProfileForm
     template_name = "edit_profile.html"
     success_url = reverse_lazy("home")
+    success_message = "Your profile has been successfully updated"
 
     def get_object(self, queryset=None):
         """Returns user object"""
@@ -86,7 +90,7 @@ class UserEditView(UpdateView):
         return context
 
 
-class PasswordsChangeView(PasswordChangeView):
+class PasswordsChangeView(SuccessMessageMixin, PasswordChangeView):
     """
     Displays User Agreement page.
     gets : requested template by name
@@ -96,6 +100,7 @@ class PasswordsChangeView(PasswordChangeView):
     form_class = PasswordChangingForm
     template_name = "change-password.html"
     success_url = reverse_lazy("home")
+    success_message = "Your password has been successfully updated"
 
     def get_context_data(self, *args, **kwargs):
         """Provides podcast categories in nav-menu."""
