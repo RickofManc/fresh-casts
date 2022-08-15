84e740d9022a9989a17bfc359b01674ff8770ea2
Rick <rickatherton316@gmail.com>
2022-08-15 15:23:39 +0000

    Update url to custom sign_up template

--- member/urls.py
+++ member/urls.py
@@ -7,7 +7,7 @@ from . import views
 
 urlpatterns = [
     path("login/", views.LoginView.as_view(), name="login"),
-    path("signup/", views.SignUpView.as_view(), name="signup"),
+    path("sign_up/", views.SignUpView.as_view(), name="sign_up"),
     path("edit_profile/", views.UserEditView.as_view(), name="edit_profile"),
     path("password/", views.PasswordsChangeView.as_view(), name="change-password"),
 ]
