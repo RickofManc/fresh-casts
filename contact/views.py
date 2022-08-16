8f98f19b7c792b2601c8cfb7eeb9a53d2c7098fd
Rick <rickatherton316@gmail.com>
2022-08-16 07:58:37 +0000

    Recode contact function to fix bug #45

--- contact/views.py
+++ contact/views.py
@@ -1,16 +1,16 @@
 """
 Django imports to support Views
 """
-from django.core.mail import send_mail, BadHeaderError
+from django.core.mail import send_mail
 from django.contrib import messages
-from django.http import HttpResponse, HttpResponseRedirect
-from django.shortcuts import render
+from django.conf import settings
+from django.shortcuts import render, redirect, reverse
 from .forms import ContactForm
 
 
 def contact(request, *args, **kwargs):
     """
-    Displays Contact Us page.
+    Displays Contact Us page form.
     Uses Post method to send contact form.
     Validation checks performed on input before saving.
     Email sent externally to Fresh Casts Gmail.
@@ -19,35 +19,28 @@ def contact(request, *args, **kwargs):
     if request.method == "POST":
         form = ContactForm(request.POST)
         if form.is_valid():
-            subject = "Website Inquiry"
-            body = {
-                "name": form.cleaned_data["name"],
-                "email": form.cleaned_data['email', ''],
-                "subject": form.cleaned_data['subject', ''],
-                "message": form.cleaned_data['message', ''],
-            }
-            message = "\n".join(body.values())
-            try:
-                messages.success(request, 'Thank you for your contacting \
-                    - Fresh Casts will reply within 24 hours!')
-                send_mail(subject, message, 'email',
-                          ["freshcastsapp@gmail.com"],
-                          fail_silently=False)
-            except BadHeaderError:
-                return HttpResponse("Invalid header found.")
-            return render(request, "contact.html", {})
+            name = form.cleaned_data["name"],
+            email = form.cleaned_data['email'],
+            subject = form.cleaned_data['subject'],
+            message = form.cleaned_data['message'],
+            
+            # send mail combining field forms
+            send_mail({subject}, f'{name}, {email}, {message}',
+                      settings.EMAIL_HOST_USER, [settings.EMAIL_HOST_USER],
+                      fail_silently=False)
+            messages.success(
+                request, 'Thank you for your contacting \
+                - Fresh Casts will reply within 24 hours!')
+            
+            # redirect to contact page
+            return redirect(reverse('contact'))
+        else:
+            messages.error(
+                request, "Something went wrong with your submission.\
+                Please try again."
+            )
+    # blank form created if any other method is used
+    else:
+        form = ContactForm()
 
-    form = ContactForm()
-    return render(request, "contact.html", {})
-
-
-def form_invalid(self):
-    """
-    Displays error to user if contact form
-    has not been successfully submitted.
-    """
-    messages.error(
-        self.request, "Something went wrong with your submission.\
-                       Please try again."
-    )
-    return HttpResponseRedirect("")
+    return render(request, 'contact.html', {'form': form})
