ff58c9eaa89516477636828ace62d217754d2c78
Rick <rickatherton316@gmail.com>
2022-08-16 07:54:58 +0000

    Include additional attributes to fix contact form success in Dev

--- fresh_casts/settings.py
+++ fresh_casts/settings.py
@@ -127,6 +127,8 @@ else:
 # Email Settings
 if 'DEVELOPMENT' in os.environ:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+    EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+    EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
     DEFAULT_FROM_EMAIL = 'freshcastsapp@gmail.com'
 else:
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
