bf1b325cea4f53c4f76d5a3ab7c0b7e1c6a51f8e
Rick <rickatherton316@gmail.com>
2022-09-20 12:40:28 +0000

    Tidy formatting and remove Author as not required

--- blog/forms.py
+++ blog/forms.py
@@ -11,33 +11,108 @@ class PostForm(forms.ModelForm):
     Uses Django forms and AddPostView
     to enable users to add blog posts
     """
-    title = forms.CharField(
-        widget=forms.TextInput(
-            attrs={'class': 'form-control',
-                   'placeholder': 'Add a title for the show here, \
-                    maybe with the episode number'})),
-    category = forms.ChoiceField(
-        widget=forms.Select(attrs={'class': 'form-control'})),
-    author = forms.ChoiceField(
-        widget=forms.Select(attrs={'class': 'form-control'})),
-    content = forms.CharField(
-        widget=forms.Textarea(
-                attrs={'class': 'form-control',
-                       'placeholder': 'Add a description of the show, and why \
-                        you are sharing it here'})),
-    excerpt = forms.CharField(
-        max_length=180, widget=forms.TextInput(
-            attrs={'class': 'form-control'})),
-    podcast_url = forms.URLInput(
-        attrs={'class': 'form-control',
-               'placeholder': 'Paste the website \
-                               address where we can listen to the show'}),
+
+    class Meta:
+        model = Post
+        fields = (
+            'title',
+            'category',
+            'content',
+            'excerpt',
+            'podcast_url',
+            'featured_image',
+        )
+
+    title = (
+        forms.CharField(
+            widget=forms.TextInput(
+                attrs={
+                    'class': 'form-control',
+                    'placeholder': 'Add a title for the show here'
+                }
+            )
+        )
+    )
+    category = (
+        forms.ChoiceField(widget=forms.Select(attrs={'class': 'form-control'})),
+    )
+    content = (
+        forms.CharField(
+            widget=forms.Textarea(
+                attrs={
+                    'class': 'form-control',
+                    'placeholder': 'Add a description of the show, and why you are sharing'
+                }
+            )
+        )
+    )
+    excerpt = (
+        forms.CharField(
+            max_length=180,
+                widget=forms.TextInput(
+                    attrs={'class': 'form-control',
+                           'placeholder': 'Add a tag line of no more than 180 characters'})
+        )
+    )
+    podcast_url = (
+        forms.URLInput(
+            attrs={'class': 'form-control'}
+        )
+    )
+    featured_image = CloudinaryFileField()
+
+
+class UpdateForm(forms.ModelForm):
+    """
+    Uses Django forms and UpdatePostView
+    to enable users to edit their own blog posts
+    """
+
+    title = (
+        forms.CharField(
+            widget=forms.TextInput(
+                attrs={
+                    'class': 'form-control',
+                    'placeholder': 'Add a title for the show here'
+                }
+            )
+        )
+    )
+    category = (
+        forms.ChoiceField(widget=forms.Select(attrs={'class': 'form-control'}))
+    )
+    content = (
+        forms.CharField(
+            widget=forms.Textarea(
+                attrs={
+                    'class': 'form-control',
+                    'placeholder': 'Add a description of the show, and why you are sharing',
+                }
+            )
+        )
+    )
+    excerpt = (
+        forms.CharField(
+            max_length=180, widget=forms.TextInput(attrs={'class': 'form-control'})
+        )
+    )
+    podcast_url = (
+        forms.URLInput(
+            attrs={'class': 'form-control'}
+        )
+    )
     featured_image = CloudinaryFileField()
 
     class Meta:
         model = Post
-        fields = ("title", "category", "author", "content",
-                  "excerpt", "podcast_url", "featured_image")
+        fields = (
+            'title',
+            'category',
+            'content',
+            'excerpt',
+            'podcast_url',
+            'featured_image',
+        )
 
 
 class CommentForm(forms.ModelForm):
@@ -46,38 +121,7 @@ class CommentForm(forms.ModelForm):
     to add comments to blog posts
     within Post Detail view.
     """
+
     class Meta:
         model = Comment
-        fields = ("message",)
-
-
-class UpdateForm(forms.ModelForm):
-    """
-    Uses Django forms and UpdatePostView
-    to enable users to edit their own blog posts
-    """
-    title = forms.CharField(
-        widget=forms.TextInput(
-            attrs={'class': 'form-control',
-                   'placeholder': 'Add a title for the show here, \
-                    maybe with the episode number'})),
-    category = forms.ChoiceField(
-        widget=forms.Select(attrs={'class': 'form-control'})),
-    content = forms.CharField(
-        widget=forms.Textarea(
-                attrs={'class': 'form-control',
-                       'placeholder': 'Add a description of the show, and why \
-                        you are sharing it here'})),
-    excerpt = forms.CharField(
-        max_length=180, widget=forms.TextInput(
-            attrs={'class': 'form-control'})),
-    podcast_url = forms.URLInput(
-        attrs={'class': 'form-control',
-               'placeholder': 'Paste the website \
-                               address where we can listen to the show'}),
-    featured_image = CloudinaryFileField()
-
-    class Meta:
-        model = Post
-        fields = ("title", "category", "content",
-                  "excerpt", "podcast_url", "featured_image")
+        fields = ('message',)
