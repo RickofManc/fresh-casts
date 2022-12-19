562943f14a5973d181d959f60c126966bb50abb5
Rick <rickatherton316@gmail.com>
2022-12-19 21:51:33 +0000

    Reformat code to ensure lines are within 79 char

--- blog/urls.py
+++ blog/urls.py
@@ -10,11 +10,35 @@ urlpatterns = [
     path("post/<slug:slug>/", views.PostDetail.as_view(), name="post_detail"),
     path("like/<slug:slug>", views.PostLike.as_view(), name="post_like"),
     path("add_post/", views.AddPostView.as_view(), name="add_post"),
-    path("post/edit/<slug:slug>", views.UpdatePostView.as_view(), name="update_post"),
-    path("post/remove/<slug:slug>", views.DeletePostView.as_view(), name="delete_post"),
-    path("category/<str:cats>/", views.CategoryView.as_view(), name="category"),
+    path(
+        "post/edit/<slug:slug>",
+        views.UpdatePostView.as_view(),
+        name="update_post"
+    ),
+    path(
+        "post/remove/<slug:slug>",
+        views.DeletePostView.as_view(),
+        name="delete_post"
+    ),
+    path(
+        "category/<str:cats>/",
+        views.CategoryView.as_view(),
+        name="category"
+    ),
     path("about/", views.About.as_view(), name="about"),
-    path("accessibility_statement/", views.AccessibilityStatement.as_view(), name="accessibility_statement",),
-    path("copyright_statement/", views.CopyrightStatement.as_view(), name="copyright_statement",),
-    path("user_agreement/", views.UserAgreement.as_view(), name="user_agreement"),
+    path(
+        "accessibility_statement/",
+        views.AccessibilityStatement.as_view(),
+        name="accessibility_statement",
+    ),
+    path(
+        "copyright_statement/",
+        views.CopyrightStatement.as_view(),
+        name="copyright_statement",
+    ),
+    path(
+        "user_agreement/",
+        views.UserAgreement.as_view(),
+        name="user_agreement"
+    ),
 ]
