4608fd596f661e72dfeec1e0e21385968823cfbc
Rick <rickatherton316@gmail.com>
2022-12-20 21:02:31 +0000

    Reformat code to ensure within char line limit

--- member/urls.py
+++ member/urls.py
@@ -9,5 +9,7 @@ urlpatterns = [
     path("login/", views.LoginView.as_view(), name="login"),
     path("sign_up/", views.SignUpView.as_view(), name="sign_up"),
     path("edit_profile/", views.UserEditView.as_view(), name="edit_profile"),
-    path("password/", views.PasswordsChangeView.as_view(), name="change-password"),
+    path("password/",
+         views.PasswordsChangeView.as_view(),
+         name="change-password"),
 ]
