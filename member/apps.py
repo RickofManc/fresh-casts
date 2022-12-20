88344d3241c55198c930e84da4e4139b049bf2a3
Rick <rickatherton316@gmail.com>
2022-12-20 21:00:47 +0000

    Remove unused import to comply with Flake8

--- member/apps.py
+++ member/apps.py
@@ -1,9 +1,10 @@
 """
-blog app configuration
+Member app configuration
 """
 from django.apps import AppConfig
 
 
 class MemberConfig(AppConfig):
+    """ Member app configuration """
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'member'
